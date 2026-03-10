3fd418e881c9e70bd94b75f81058d1f9923ebba3
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-03-09 23:32:15 -0400

    Assignment 3 - String methods

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week5 assignment3
+"""
+text1 = "This is a simple test"
+count_is = text1.count("is")
+print('The substring "is" appears', count_is, "times.")
+text2 = "This is an old example."
+updated_text = text2.replace("old", "new")
+print(updated_text)
