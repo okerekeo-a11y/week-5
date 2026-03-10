fa15b9217fdd6b5049cb61192df67e7c7552e43b
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-03-09 23:27:00 -0400

    Assignment 1 - String slicing functions

---
+++ string_slicing.py
@@ -0,0 +1,13 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week5 assignment1
+"""
+def first_three(text):
+    return text[:3]
+def reverse_string(text):
+    return text[::-1]
+word = "Python"
+print("First three characters:", first_three(word))
+print("Reversed string:", reverse_string(word))
